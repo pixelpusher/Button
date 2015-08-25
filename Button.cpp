6be667ee7243af1b9c5f39c623d51f126ec046aa
Lee Thomason <leethomason@gmail.com>
2015-08-24 23:09:35 -0700

    fix minor type bugs. code now materially changed; removing licence.

--- Button.cpp
+++ Button.cpp
@@ -1,18 +1,3 @@
-/* $Id$
-||
-|| @file 		       Button.cpp
-|| @author 		     Alexander Brevig              <alexanderbrevig@gmail.com>        
-|| @url            http://alexanderbrevig.com
-||
-|| @description
-|| | This is a Hardware Abstraction Library for Buttons
-|| | It provides an easy way of handling buttons
-|| #
-||
-|| @license LICENSE_REPLACE
-||
-*/
-
 #include <Arduino.h>
 #include "Button.h"
 
@@ -23,26 +8,11 @@
 
 //#define DEBUG_SERIAL
 
-/*
-|| @constructor
-|| | Set the initial state of this button
-|| #
-|| 
-|| @parameter buttonPin  sets the pin that this switch is connected to
-|| @parameter buttonMode indicates BUTTON_PULLUP or BUTTON_PULLDOWN resistor
-*/
-Button::Button(uint8_t buttonPin, uint8_t buttonMode, bool _debounceMode, int _debounceDuration)
-{
-  init(buttonPin, buttonMode, _debounceMode, _debounceDuration);
-}
-
-
-void Button::init(uint8_t buttonPin, uint8_t buttonMode, bool _debounceMode, int _debounceDuration)
+Button::Button(uint8_t buttonPin, uint8_t buttonMode, uint32_t _debounceDuration)
 {
 	myPin=buttonPin;
   pinMode(myPin,INPUT);
   
-  debounceMode = _debounceMode;
   debounceDuration = _debounceDuration;
   debounceStartTime = millis();
 
@@ -63,44 +33,25 @@ void Button::init(uint8_t buttonPin, uint8_t buttonMode, bool _debounceMode, int
   triggeredHoldEvent = true;
   holdEventThreshold = DEFAULT_HOLD_TIME;
 
-  #ifdef DEBUG_SERIAL
+#ifdef DEBUG_SERIAL
   Serial.print("Button init:");
   Serial.println(buttonPin);
-  #endif
+#endif
 }
 
-/*
-|| @description
-|| | Prepare logic for a pullup button.
-|| | If mode is internal set pin HIGH as default
-|| #
-*/
+
 void Button::pullup(uint8_t buttonMode)
 {
 	mode=BUTTON_PULLUP;
-  if (buttonMode == BUTTON_PULLUP_INTERNAL) 
-  {
-	  digitalWrite(myPin,HIGH);
-  }
 }
 
-/*
-|| @description
-|| | Set pin LOW as default
-|| #
-*/
+
 void Button::pulldown(void)
 {
 	mode=BUTTON_PULLDOWN;
 }
 
-/*
-|| @description
-|| | Read and write states; issue callbacks
-|| #
-|| 
-|| @return true if button is pressed
-*/
+
 void Button::process(void)
 {
   //save the previous value
@@ -116,7 +67,7 @@ void Button::process(void)
   {
     uint32_t interval = currentMillis - debounceStartTime;
 
-    if(debounceMode && interval < debounceDuration){
+    if(interval < debounceDuration){
       // not enough time has passed; ignore
       return;
     }
@@ -149,7 +100,7 @@ void Button::process(void)
         if (cb_onClick) { cb_onClick(*this); }   //fire the onClick event AFTER the onRelease
       }
       //reset states (for timing and for event triggering)
-      pressedStartTime = -1;
+      pressedStartTime = uint32_t(-1);
     }
     //note that the state changed
     bitWrite(state,CHANGED,true);
@@ -159,7 +110,7 @@ void Button::process(void)
     //note that the state did not change
     bitWrite(state,CHANGED,false);
     //should we trigger an onHold event?
-    if (pressedStartTime != -1 && !triggeredHoldEvent && bitRead(state, CURRENT)) 
+    if (pressedStartTime != uint32_t(-1) && !triggeredHoldEvent && bitRead(state, CURRENT)) 
     {
       if (currentMillis - pressedStartTime > holdEventThreshold) 
       { 
@@ -173,56 +124,31 @@ void Button::process(void)
   }
 }
 
-/*
-|| @description
-|| | Return the bitRead(state,CURRENT) of the switch
-|| #
-|| 
-|| @return true if button is pressed
-*/
+
 bool Button::pressed() const
 {
 	return bitRead(state,CURRENT);
 }
 
-/*
-|| @description
-|| | Return true if state has been changed
-|| #
-*/
+
 bool Button::stateChanged() const
 {
   return bitRead(state,CHANGED);
 }
 
-/*
-|| @description
-|| | Return true if the button is pressed, and was not pressed before
-|| #
-*/
+
 bool Button::uniquePress() const
 {
   return (pressed() && stateChanged());
 }
 
-/*
-|| @description
-|| | onHold polling model
-|| | Check to see if the button has been pressed for time ms
-|| | This will clear the counter for next iteration and thus return true once
-|| #
-*/
+
 bool Button::held() const
 {
-  return (pressedStartTime != -1) && triggeredHoldEvent; 
+  return (pressedStartTime != uint32_t(-1)) && triggeredHoldEvent; 
 }
 
-/*
-|| @description
-|| | Polling model for holding, this is true every check after hold time
-|| | Check to see if the button has been pressed for time ms
-|| #
-*/
+
 uint32_t Button::holdTime() const
 {
   if (!pressed())
@@ -230,59 +156,31 @@ uint32_t Button::holdTime() const
   return millis() - pressedStartTime;
 }
 
-/*
-|| @description
-|| | Set the hold event time threshold
-|| #
-*/
+
 void Button::setHoldThreshold(uint32_t holdTime) 
 { 
   holdEventThreshold = holdTime; 
 }
 
-/*
-|| @description
-|| | Register a handler for presses on this button
-|| #
-||
-|| @parameter handler The function to call when this button is pressed
-*/
+
 void Button::pressHandler(buttonEventHandler handler)
 {
   cb_onPress = handler;
 }
 
-/*
-|| @description
-|| | Register a handler for releases on this button
-|| #
-||
-|| @parameter handler The function to call when this button is released
-*/
+
 void Button::releaseHandler(buttonEventHandler handler)
 {
   cb_onRelease = handler;
 }
 
-/*
-|| @description
-|| | Register a handler for clicks on this button
-|| #
-||
-|| @parameter handler The function to call when this button is clicked
-*/
+
 void Button::clickHandler(buttonEventHandler handler)
 {
   cb_onClick = handler;
 }
 
-/*
-|| @description
-|| | Register a handler for when this button is held
-|| #
-||
-|| @parameter handler The function to call when this button is held
-*/
+
 void Button::holdHandler(buttonEventHandler handler, uint32_t holdTime)
 {
   if (holdTime>0) { setHoldThreshold(holdTime); }
--- Button.h
+++ Button.h
@@ -1,25 +1,9 @@
-/* $Id$
-||
-|| @file 		       Button.cpp
-|| @author 		     Alexander Brevig              <alexanderbrevig@gmail.com>        
-|| @url            http://alexanderbrevig.com
-||
-|| @description
-|| | This is a Hardware Abstraction Library for Buttons
-|| | It providea an easy way of handling buttons
-|| #
-||
-|| @license LICENSE_REPLACE
-||
-*/
-
 #ifndef Button_h
 #define Button_h
 
 #include <inttypes.h>
 
 #define BUTTON_PULLUP           HIGH
-#define BUTTON_PULLUP_INTERNAL  2
 #define BUTTON_PULLDOWN         LOW
 
 class Button;
@@ -29,20 +13,33 @@ class Button {
   public:
 
     enum {
-        DEFAULT_HOLD_TIME = 500
+        DEFAULT_HOLD_TIME = 500,
+        DEFAULT_BOUNCE_DURATION = 20
     };    
   
-    Button(uint8_t buttonPin, uint8_t buttonMode=BUTTON_PULLUP_INTERNAL, bool _debounceMode=true, int _debounceDuration=20);
+    /**
+     *  Construct a button.
+     *  'buttonPin' is the pin for this button to use.
+     *  'buttonMode' is the wiring of the button. (Referring to the resistor.)
+     *      - BUTTON_PULLDOWN is 'buttonPin' connected (through a resistor) to ground. When pressed, the button goes high.
+     *      - BUTTON_PULLUP is 'buttonPin' connected (through a resistor) to high. When pressed, the button goes low.
+     *  'debounceDuration' is how long it takes the button to settle, mechanically, when pressed.
+     */
+    Button(uint8_t buttonPin, uint8_t buttonMode=BUTTON_PULLUP, uint32_t debounceDuration = DEFAULT_BOUNCE_DURATION);
     
     int  pin() const {return myPin; }
-    void pullup(uint8_t buttonMode);
-    void pulldown();
+
     void process();
 
+    /// true if pressed
     bool pressed() const;
+    /// true if changed at last process() 
     bool stateChanged() const;
+    /// true if the press was at last process()
     bool uniquePress() const;
+    /// true if the button is held to the trigger time or longer
     bool held() const;
+    /// return the time the button held down
     uint32_t holdTime() const;
     
     void setHoldThreshold(uint32_t holdTime);
@@ -55,12 +52,12 @@ class Button {
     inline uint16_t presses() const { return numberOfPresses; }
     
   private: 
-    void init(uint8_t buttonPin, uint8_t buttonMode=BUTTON_PULLUP_INTERNAL, bool _debounceMode=true, int _debounceDuration=20);
+    void pullup(uint8_t buttonMode);
+    void pulldown();
 
     uint8_t             myPin;
     uint8_t             mode;
     uint8_t             state;
-    bool                debounceMode;
     uint32_t            pressedStartTime;
     uint32_t            holdEventThreshold;
     uint32_t            debounceStartTime;
